15d7c16bff9f3c6c0964cf6bd024fe99c583e170
stopaalex <stopa.alex@gmail.com>
2018-04-25 13:04:09 -0400

    UPDATE :::
    fixing promise issue - displaying weather for  defualt location

--- scripts.js
+++ scripts.js
@@ -1,37 +1,57 @@
-'use strict';
 
-const cityName = document.querySelector('#cityName');
-
-var zip = '22201';
-var lat = 0;
-var long = 0;
 var tempType = 'imperial';
 
-// var weather = {}
-var location = {};
+const cityInfo = document.querySelector('#cityInfo');
+const weatherText = document.querySelector('#weatherText .weather-type');
+const weatherImg = document.querySelector('#weatherImg img');
+const zipBtn = document.querySelector('#searchZip');
+var zip = document.querySelector('#zipCodeSearch').value;
 
-var url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',us&units=' + tempType + '&appid=d47f5e0ca7fe361331cb5e11a3c95a30';
-var url2 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zip + '&key=AIzaSyDalUj2S8wriBt-HGwa-si6xUYKoCxR11s';
 
-function getZipData(weatherData) {
+function getZipData() {
+    var url2 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zip + '&key=AIzaSyDalUj2S8wriBt-HGwa-si6xUYKoCxR11s';
     fetch(url2)
     .then(function(blob) {
         return blob.json()
         .then(function(data) {
-            location = data.results[0];
-            console.log(data.results[0]);
+            var location = data.results[0];
+            console.log(location);
+            
+            var city = location.address_components[1].short_name;
+            var state = location.address_components[3].short_name;
+
+            cityInfo.textContent = city + ', ' + state;
+
+            getWeather(location.address_components[0].short_name);
         });
     });
 }
 
-function getWeather() {
+function getWeather(zipCode) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&units=' + tempType + '&appid=d47f5e0ca7fe361331cb5e11a3c95a30';
     fetch(url)
     .then(function(blob) {
         return blob.json()
         .then(function(data) {
-            getZipData(data);
+            var weather = data;
+            var weatherType = weather.weather[0].main;
+            weatherText.textContent = weatherType;
+            weatherImg.src = 'icons/' + weatherType.toLowerCase() + '.svg';
         });
     });
 }
 
-getZipData();
+function getWeatherFromZip() {
+    var zip = document.querySelector('#zipCodeSearch').value;
+    console.log(zip);
+}
+
+function initialize() {
+    if (!zip) {
+        zip = '22201';
+    }
+    zipBtn.addEventListener('click', getWeatherFromZip);
+    getZipData();
+}
+
+initialize();
