87a88a7429e2d1a933d4c84a8d521c0edd7f5c0e
stopaalex <stopa.alex@gmail.com>
2018-04-25 14:31:36 -0400

    UPDATE :::
    with temps and updated icons

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 
 var tempType = 'imperial';
 
+const card = document.querySelector('.card');
+
 const cityInfo = document.querySelector('#cityInfo');
+// Weather
 const weatherText = document.querySelector('#weatherText .weather-type');
 const weatherImg = document.querySelector('#weatherImg img');
+// Temps
+const temps = document.querySelector('#temps');
+const currentTempCont = document.querySelector('#temps .current-temp');
+const highTempCont = document.querySelector('#temps .highs-lows .temp-high');
+const lowTempCont = document.querySelector('#temps .highs-lows .temp-low');
+
 const zipBtn = document.querySelector('#searchZip');
 var zip;
 
@@ -16,33 +25,61 @@ function getZipData(zipCode) {
         var url2 = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + zipCode + '&key=AIzaSyDalUj2S8wriBt-HGwa-si6xUYKoCxR11s';
     }
     fetch(url2)
-    .then(function(blob) {
-        return blob.json()
-        .then(function(data) {
-            var location = data.results[0];
-            
-            var city = location.address_components[1].short_name;
-            var state = location.address_components[3].short_name;
+        .then(function (blob) {
+            return blob.json()
+                .then(function (data) {
+                    var location = data.results[0];
 
-            cityInfo.textContent = city + ', ' + state;
+                    var address = location.formatted_address;
 
-            getWeather(location.address_components[0].short_name);
+                    cityInfo.textContent = address;
+
+                    if (!zipCode) {
+                        getWeather(location.address_components[0].short_name, true);
+                    } else if (isNaN(zipCode)) {
+                        getWeather(location.address_components[0].short_name, false);
+                    } else {
+                        getWeather(zipCode, true);
+                    }
+                });
         });
-    });
 }
 
-function getWeather(zipCode) {
-    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&units=' + tempType + '&appid=d47f5e0ca7fe361331cb5e11a3c95a30';
+function getWeather(zipCode, checkForNum) {
+    if (!checkForNum) {
+        var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + zipCode + ',us&units=' + tempType + '&appid=d47f5e0ca7fe361331cb5e11a3c95a30';
+    } else {
+        var url = 'http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&units=' + tempType + '&appid=d47f5e0ca7fe361331cb5e11a3c95a30';
+    }
     fetch(url)
-    .then(function(blob) {
-        return blob.json()
-        .then(function(data) {
-            var weather = data;
-            var weatherType = weather.weather[0].main;
-            weatherText.textContent = weatherType;
-            weatherImg.src = 'icons/' + weatherType.toLowerCase() + '.svg';
+        .then(function (blob) {
+            return blob.json()
+                .then(function (data) {
+                    var weather = data;
+                    // Display Weather
+                    var weatherType = weather.weather[0].main;
+                    weatherText.textContent = weatherType;
+                    weatherImg.src = 'icons/' + weatherType.toLowerCase() + '.svg';
+                    console.log(weather);
+                    
+                    // Display Temps
+                    var currentTemp = Math.round(weather.main.temp);
+                    var highTemp = Math.round(weather.main.temp_max);
+                    var lowTemp = Math.round(weather.main.temp_min);
+                    currentTempCont.textContent = currentTemp + '°F';
+                    highTempCont.textContent = highTemp + '°F';
+                    lowTempCont.textContent = lowTemp + '°F';
+
+                    // Update Card Color based on cloudiness
+                    var cloudiness = 100 - weather.clouds.all;
+                    card.style.background = 'hsla(200, ' + cloudiness + '%, 93%, 0.8)'
+                    console.log(cloudiness);
+
+                    // Clear input
+                    document.querySelector('#zipCodeSearch').value = '';
+
+                });
         });
-    });
 }
 
 function getWeatherFromZip() {
@@ -57,7 +94,7 @@ function isEnterPressed(e) {
     } else {
         return
     }
-    
+
 }
 
 function initialize() {
